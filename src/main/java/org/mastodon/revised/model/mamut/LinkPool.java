477b9a87e65eb5a87846fda0ae028ee533c8898e
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2017-05-08 22:33:55 +0200

    Enfore the `ModelGraph` to be a simple, directed graph.
    Since the edge pool in the model graph extends
    `AbstractListenableEdgePool`, it is non-simple. We do not
    want that first because this is not our target application
    (following cells over time), second because we want to build
    a branch graph on this model graph, and that the branch-graph
    is only defined for simple graphs.

--- src/main/java/org/mastodon/revised/model/mamut/LinkPool.java
+++ src/main/java/org/mastodon/revised/model/mamut/LinkPool.java
@@ -2,6 +2,7 @@ package org.mastodon.revised.model.mamut;
 
 import org.mastodon.graph.ref.AbstractEdgePool;
 import org.mastodon.graph.ref.AbstractListenableEdgePool;
+import org.mastodon.graph.ref.AbstractVertex;
 import org.mastodon.pool.ByteMappedElement;
 import org.mastodon.pool.ByteMappedElementArray;
 import org.mastodon.pool.SingleArrayMemPool;
@@ -17,5 +18,62 @@ public class LinkPool extends AbstractListenableEdgePool< Link, Spot, ByteMapped
 	protected Link createEmptyRef()
 	{
 		return new Link( this );
+	}
+
+	/**
+	 * Adds a link between the specified source and target.
+	 * <p>
+	 * If a link already exists between this source and target (with this
+	 * direction), the link is not added and this method returns
+	 * <code>null</code>.
+	 *
+	 * @param source
+	 *            the source vertex.
+	 * @param target
+	 *            the target vertex.
+	 * @param edge
+	 *            a reference object used for operation.
+	 * @return the added link, or <code>null</code> if an edge already exists
+	 *         between source and target.
+	 */
+	@Override
+	public Link addEdge( final AbstractVertex< ?, ?, ?, ? > source, final AbstractVertex< ?, ?, ?, ? > target, final Link edge )
+	{
+		if ( getEdge( source, target, edge ) != null )
+			return null;
+
+		return super.addEdge( source, target, edge );
+	}
+
+	/**
+	 * Inserts an edge between the specified source and target, at the specified
+	 * positions in the edge lists of the source and target vertices.
+	 * <p>
+	 * If an edge already exists between this source and target (with this
+	 * direction), the edge is not added and this method returns
+	 * <code>null</code>.
+	 *
+	 * @param source
+	 *            the source vertex.
+	 * @param sourceOutInsertAt
+	 *            the position the created edge is to be inserted in the source
+	 *            vertex outgoing edge list.
+	 * @param target
+	 *            the target vertex.
+	 * @param targetInInsertAt
+	 *            the position the created edge is to be inserted in the target
+	 *            vertex incoming edge list.
+	 * @param edge
+	 *            a reference object used for operation.
+	 * @return the added edge, or <code>null</code> if an edge already exists
+	 *         between source and target.
+	 */
+	@Override
+	public Link insertEdge( final AbstractVertex< ?, ?, ?, ? > source, final int sourceOutInsertAt, final AbstractVertex< ?, ?, ?, ? > target, final int targetInInsertAt, final Link edge )
+	{
+		if ( getEdge( source, target, edge ) != null )
+			return null;
+
+		return super.insertEdge( source, sourceOutInsertAt, target, targetInInsertAt, edge );
 	}
 }
